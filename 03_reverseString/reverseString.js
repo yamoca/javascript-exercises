baff540349ae672d18bdfce164ba72ee408577fb
leo <leojwgulliver@gmail.com>
2024-07-23 18:17:02 +0100

    completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(inputStr) {
+    // you can reverse an array so can just do inputStr.split("").reverse().join("");
+    let inputArray = inputStr.split("");
+    let len = inputArray.length;
+    let reversedArray = [];
+    for(let i = 0; i <= len; i++) {
+        reversedArray.unshift(inputArray[i]);
+    }
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
