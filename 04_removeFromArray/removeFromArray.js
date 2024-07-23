6d1cd40f5aaf66456f84dfc5dc86fad1c40cf719
leo <leojwgulliver@gmail.com>
2024-07-23 19:48:41 +0100

    completed 04 (possibly clunky)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArr, ...args) {
+    let arrLen = inputArr.length;
+    let argsLen = args.length;
+    
+    for (let i = 0; i < argsLen; i++) {
+        for (let j = 0; j < arrLen; j++) {
+            if (inputArr[j] === args[i]) {
+                inputArr.splice(j, 1);
+                arrLen--;
+                j--;
+            }
+    }
+
+
+    }
+    return inputArr;
 };
 
 // Do not edit below this line
