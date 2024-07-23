ce5ff3212b56bde7872bee9ec231576c0a0933ed
leo <leojwgulliver@gmail.com>
2024-07-23 20:18:49 +0100

    remove unnecesary syntax (convert to one-line if and for statements etc)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum = 0;
 
+    if (Number.isInteger(min) && Number.isInteger(max) && min > -1 && max > -1) {
+        if (min > max) [min, max] = [max, min];
+        for(let i = min; i <= max; i++) sum += i;
+        return sum;
+    } else return "ERROR";
+    
 };
 
 // Do not edit below this line
