2358d8ff5971847d5d1ee9d0fe513cd8c1c8c76f
leo <leojwgulliver@gmail.com>
2024-07-23 20:00:03 +0100

    take alternate approach (according to solution) for 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,29 @@
 const removeFromArray = function(inputArr, ...args) {
-    let arrLen = inputArr.length;
-    let argsLen = args.length;
+    // let arrLen = inputArr.length;
+    // let argsLen = args.length;
     
-    for (let i = 0; i < argsLen; i++) {
-        for (let j = 0; j < arrLen; j++) {
-            if (inputArr[j] === args[i]) {
-                inputArr.splice(j, 1);
-                arrLen--;
-                j--;
-            }
-    }
+    // for (let i = 0; i < argsLen; i++) {
+    //     for (let j = 0; j < arrLen; j++) {
+    //         if (inputArr[j] === args[i]) {
+    //             inputArr.splice(j, 1);
+    //             arrLen--;
+    //             j--;
+    //         }
+    //     }
+    // }
+    // return inputArr;
 
+    // create a new array and push items that are not in args (instead of removing undesired items from inputarr)
+    const outputArr = []
+    // iterate more cleanly
+    inputArr.forEach(item => {
+        if (!args.includes(item)) {
+            outputArr.push(item)
+        }
+    });
 
-    }
-    return inputArr;
+    return outputArr;
+
 };
 
 // Do not edit below this line
