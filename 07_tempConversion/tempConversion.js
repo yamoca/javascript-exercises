9cdb56e3e096ae6eca4980db82769475732ec554
leo <leojwgulliver@gmail.com>
2024-07-24 16:47:28 +0100

    complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheight) {
+  const celcius = (fahrenheight-32) * 5/9
+  return Math.round(celcius*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  const fahrenheight = celcius * 9/5 + 32
+  return Math.round(fahrenheight*10)/10; // hack to round to 1 dp instead of nearest whole number
 };
 
 // Do not edit below this line
