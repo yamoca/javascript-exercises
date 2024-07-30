db9aec3d0644cbde312411c192a96694142d8d32
leo <leojwgulliver@gmail.com>
2024-07-30 20:43:20 +0100

    finish 08 calc (its just slang)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, item) => total += item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => total * item, 1);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  return base**power; 
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  const nums = [];
+  for (let i = 1; i <= num; i++) { 
+    nums.push(i) 
+  };
+
+  return nums.reduce((total, item) => total * item, 1);
 };
 
 // Do not edit below this line
