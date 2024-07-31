a0d2e9150aea6a80a58ec43d2b79e40d268ad299
leo <leojwgulliver@gmail.com>
2024-07-31 17:31:40 +0100

    finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
+function getAge(person) {
+    person.yearOfDeath ??= new Date().getFullYear();
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function(arr) {
+    return arr.sort((a, b) => getAge(b) - getAge(a))[0];
 };
 
 // Do not edit below this line
