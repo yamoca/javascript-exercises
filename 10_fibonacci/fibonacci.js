3d6c8019712e867a829e077d5f44c48b55d105d8
leo <leojwgulliver@gmail.com>
2024-07-31 16:06:30 +0100

    complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(iterations) {
+    let start = 0;
+    let next = 1;
+    let temp = 0;
+    if (iterations >= 0) {
+        for (let i = 0; i < iterations; i++) {
+            temp = start + next;
+            start = next;
+            next = temp;
+        }
+        return start;
+    } else {
+        return "OOPS";
+    }
 
 };
 
