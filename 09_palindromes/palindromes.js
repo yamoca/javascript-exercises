732fd027a167dbec559a5df59053255a31ece0da
leo <leojwgulliver@gmail.com>
2024-07-31 15:51:55 +0100

    finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
 
+const palindromes = function (word) {
+    word = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    return word === word.split('').reverse().join('');
 };
 
 // Do not edit below this line
